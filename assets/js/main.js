8ba98efb1f550a70d7123688ded63674acb13818
mateussantanasilva <santanasilva1778@gmail.com>
2022-08-28 14:29:41 -0300

    :lipstick: estrutura básica de páginas públicas - header e footer

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+// =============== INITIAL SETTINGS ===============
+window.addEventListener('scroll', onScroll)
+const bodyElement = document.body
+
+function onScroll() {
+
+    const menu = document.querySelector('#header')
+    const AlreadyHaveClass = menu.classList.contains('fixed-menu')
+
+    if((scrollY > 10) && (!AlreadyHaveClass)) {
+        changeColorMenuOnScroll(menu)
+    }
+
+    if((scrollY <= 0) && (AlreadyHaveClass)){
+        menu.classList.remove('fixed-menu')
+    }
+}
+
+// =============== MENU ===============
+(function activeMenuOnCurrentPage(){
+    const page = `.${location.pathname}`
+
+    const headerListOption = document.querySelector(`.header-list_option[href="${page}"]`)
+    if(headerListOption.textContent == 'cadastre-se') {
+        headerListOption.classList.add('menu-active-btn')
+    }
+    headerListOption.classList.add('menu-active')
+})()
+
+function changeColorMenuOnScroll(menu) {
+    menu.classList.add('fixed-menu')
+}
+
+const burger = document.querySelector('.burger')
+const menuMobile = document.querySelector('.menu-mobile')
+
+burger.addEventListener('click', () => openMobileMenu(burger))
+
+function openMobileMenu(burger){
+    burger.classList.toggle('open')
+    menuMobile.classList.toggle('active-menu-mobile')
+    bodyElement.classList.toggle('active-menu-mobile')
+}
