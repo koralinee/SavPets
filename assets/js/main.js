27952e136b54fa98a545a78708ff6f12caef2a1a
mateussantanasilva <santanasilva1778@gmail.com>
2022-09-04 15:56:32 -0300

    :lipstick: home criada com finalização da base do projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,11 +33,36 @@ function changeColorMenuOnScroll(menu) {
 
 const burger = document.querySelector('.burger')
 const menuMobile = document.querySelector('.menu-mobile')
+const containerWelcome = document.querySelector('.presentation-container_welcome')
 
 burger.addEventListener('click', () => openMobileMenu(burger))
 
 function openMobileMenu(burger){
+    containerWelcome.setAttribute('style', '')
     burger.classList.toggle('open')
     menuMobile.classList.toggle('active-menu-mobile')
     bodyElement.classList.toggle('active-menu-mobile')
 }
+
+// =============== SCROLL REVEAL SETTINGS ===============
+const scrollReveal = ScrollReveal({
+    origin: 'left',
+    distance: '40px',
+    duration: '800',
+    delay: 150
+})
+
+scrollReveal.reveal(`
+    .presentation-container_welcome,
+    .optionshelp-container_title,
+    .optionshelp-group_card,
+    .about-container,
+    .difference-container_description,
+    .difference-highlights_item,
+    .start-container,
+    .contact-container
+`)
+
+scrollReveal.reveal(`
+    .footer-container
+`, {origin: 'top'})
