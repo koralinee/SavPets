d77908bd309ac7837535038a8a6542a76c3d6e4d
mateussantanasilva <santanasilva1778@gmail.com>
2022-09-18 22:16:12 -0300

    :lipstick: base visual do crud

--- assets/js/main-management.js
+++ assets/js/main-management.js
@@ -1,8 +1,8 @@
 // =============== MENU ===============
-(function activeMenuOnCurrentPage(){
+(function activeMenuOnCurrentPage() {
     const page = `./${document.body.classList}.html`
     const headerListOption = document.querySelector(`.header-content_menu a[href="${page}"]`)
-    
+
     headerListOption.classList.add('menu-management-active')
 })()
 
@@ -11,14 +11,80 @@ const headerContent = document.querySelector('.header-content')
 
 burger.addEventListener('click', () => openMobileMenu(burger))
 
-function openMobileMenu(burger){
+function openMobileMenu(burger) {
     burger.classList.toggle('open')
     headerContent.classList.toggle('side-menu--management-active')
 }
+
+// =============== QUIT OPTIONS SETTINGS ===============
+const quitOption = document.querySelectorAll('.option-quit')
+
+quitOption.forEach(option => {
+    option.addEventListener('click', (event) => {
+        event.preventDefault()
+        Swal.fire({
+            title: 'Atenção',
+            text: "Você deseja sair da sessão?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#a4cbe0',
+            cancelButtonColor: '#ff7575',
+            cancelButtonText: 'Cancelar',
+            confirmButtonText: 'Sim, sair!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+                })
+                Toast.fire({
+                icon: 'warning',
+                title: 'Saindo da sessão'
+                })
+    
+                setTimeout(() => window.location.href = "../index.html", 3100)
+            }
+        })
+    })
+})
 
 // =============== DATATABLES SETTINGS ===============
 $(document).ready(function () {
     $('#example').DataTable({
         responsive: true
     });
+});
+
+// =============== ACTIONS TABLE SETTINGS ===============
+const deleteBtn = document.querySelectorAll('.column-action_delete')
+console.log(deleteBtn)
+
+deleteBtn.forEach(btn => {
+    btn.addEventListener('click', () =>{
+        Swal.fire({
+            title: 'Tem certeza?',
+            text: "Você não poderá reverter isso!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#a4cbe0',
+            cancelButtonColor: '#ff7575',
+            cancelButtonText: 'Cancelar',
+            confirmButtonText: 'Sim, deletar!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Deletado!',
+                    'Registro apagado com sucesso.',
+                    'success'
+                )
+            }
+        })
+    })
 });
